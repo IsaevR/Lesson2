82c85cb13fafede175b09bfbe7494a4dd49823ac
IsaevR <diplomatic.area@gmail.com>
2018-04-11 15:16:45 +0300

    third commit

--- Parser.cs
+++ Parser.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ConsoleApp9
+namespace Parser
 {
     class Program
     {
@@ -10,14 +10,20 @@ namespace ConsoleApp9
             while (true)
             {
                 
-                var s = Console.ReadLine();
-                var datestart = DateTime.Now;
-                Console.WriteLine(Parse(s));
-                Console.WriteLine(DateTime.Now-datestart);
+                var s = Console.ReadLine();      // записывает строку в переменной "s"
+                var datestart = DateTime.Now;    // сохраняет текущее время в переменной "datestart" 
+                Console.WriteLine(Parse(s));     
+                Console.WriteLine(DateTime.Now-datestart); // вычитает время старта программы от времени 
+                                                           // его завершения 
                
             }
             
         }
+        /// <summary>
+        /// метод Parse выполняет сложение и вычитание
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         static int Parse(string s)
         {
             int index = 0;
@@ -42,13 +48,44 @@ namespace ConsoleApp9
                     return 0;
                 }
             }
+            return num;
+        }
+        /// <summary>
+        /// Метод MultDiv выполняет умножение и деление
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static int MultDiv(string s)
+        {
+            int index = 0;
+            int num = Num(s, ref index);
+            while (index < s.Length)
+            {
+                if (s[index] == '*')
+                {
+                    index++;
+                    int b = Num(s, ref index);
+                    num *= b;
+                }
+                else if (s[index] == '/')
+                {
+                    index++;
+                    int b = Num(s, ref index);
+                    num /= b;
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                    return 0;
+                }
+            }
 
             return num;
         }
 
         static int Num(string s, ref int i)
         {
-            string buff = "0";
+            string buff = "0"; 
             for (; i < s.Length && char.IsDigit(s[i]); i++)
             {
                 buff += s[i];
